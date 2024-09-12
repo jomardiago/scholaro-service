ce52971ea0d0bfccd2fe67c5a828a8297a031fa6
Jose <jomardiago@gmail.com>
2024-09-12 14:03:10 +0800

    Initial setup.

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcrypt";
+
+const prisma = new PrismaClient();
+const password = "password12345!";
+
+async function main() {
+  const hashedPassword = await hash(password, 10);
+
+  const josediago = await prisma.user.upsert({
+    where: { email: "admin@email.com" },
+    update: {},
+    create: {
+      email: "admin@email.com",
+      password: hashedPassword,
+      isAdmin: true,
+    },
+  });
+
+  console.log({ josediago });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
